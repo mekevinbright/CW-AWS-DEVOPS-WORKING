3eb8a7f6475c3897a2edb2225e8a3f66c24b85cb
kevin <mekevinbright@gmail.com>
2020-09-30 14:34:54 +0300

    Miliseconds app.py dosyası güncellendi

--- aws/projects/002-milliseconds-converter/milisec_app.py
+++ aws/projects/002-milliseconds-converter/milisec_app.py
@@ -30,6 +30,6 @@ def convertmili():
       
     
 if __name__ == "__main__":
-    app.run(debug = True)
+    # app.run(debug = True)
 
-    # app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
